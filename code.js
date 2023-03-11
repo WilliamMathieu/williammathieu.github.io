e0388d7dbb8f37b22f26f225d9e146e214c35a03
WilliamMathieu <3903679+WilliamMathieu@users.noreply.github.com>
2023-03-11 00:32:18 -0500

    Update code.js

--- code.js
+++ code.js
@@ -2,5 +2,5 @@ const btn= document.getElementById("btn");
 
 btn.addEventListener('click', function(){
   var name = document.getElementById("myName").value;
-  alert("Name: "+ name);
+  document.getElementById("name").innerHTML = "Your text is: "+ name;
 });
