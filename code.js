9733549ce9ed1ee2f42b04afad4e4aa7535060d3
WilliamMathieu <3903679+WilliamMathieu@users.noreply.github.com>
2023-03-14 20:56:40 -0400

    Update code.js

--- code.js
+++ code.js
@@ -1,6 +1,7 @@
 const btn= document.getElementById("btn");
 
 btn.addEventListener('click', function(){
-  var name = document.getElementById("myName").value;
-  document.getElementById("name").innerHTML = "Your text is: "+ name;
+  var num1 = document.getElementById("num1").value;
+  var num2 = document.getElementById("num2").value;
+  document.getElementById("result").innerHTML = "Result = "+ num1*num2;
 });
