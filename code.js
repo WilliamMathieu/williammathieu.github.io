0a6d56efb11111ad854ebc8a52c9cc2f40d6a93a
WilliamMathieu <3903679+WilliamMathieu@users.noreply.github.com>
2023-03-16 17:23:40 -0400

    Update code.js

--- code.js
+++ code.js
@@ -3,5 +3,5 @@ const btn= document.getElementById("btn");
 btn.addEventListener('click', function(){
   var f = document.getElementById("f").value;
   var L = document.getElementById("L").value;
-  document.getElementById("C").innerHTML = "Result = "+ (1/(4*(Math.PI**2)*(f**2)*L)) +"F";
+  document.getElementById("C").innerHTML = "Result = "+ (1/(4*(Math.PI**2)*(f**2)*L)) +" F";
 });
