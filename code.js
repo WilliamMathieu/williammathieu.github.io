ee8d28d5cbbdd8191f13759aa3c3e631f58d3a67
WilliamMathieu <3903679+WilliamMathieu@users.noreply.github.com>
2023-03-23 22:42:31 -0400

    Delete code.js

--- code.js
+++
@@ -1,7 +0,0 @@
-const btn= document.getElementById("btn");
-
-btn.addEventListener('click', function(){
-  var f = document.getElementById("f").value;
-  var L = document.getElementById("L").value;
-  document.getElementById("C").innerHTML = "C = "+ (1/(4*(Math.PI**2)*(f**2)*L)) +" F";
-});
